0cfd68860a146f7d4b7f31a04d36c042347e0aa9
Chuluq <choirulchuluq10@gmail.com>
2025-11-10 19:34:16 +0700

    dynamic attribute

--- src/lib/HelloSvelte.svelte
+++ src/lib/HelloSvelte.svelte
@@ -1,8 +1,10 @@
 <script>
   const name = "Svelte";
+  const src = "https://www.programmerzamannow.com/img/pzn.png";
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+<img {src} alt="pzn-logo" />
 
 <style>
   h1 {
