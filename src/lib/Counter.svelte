9477eabc0ddbe499bcaeeb5779841f5abbbace54
Chuluq <choirulchuluq10@gmail.com>
2025-11-11 11:03:30 +0700

    derived by

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,6 +1,13 @@
 <script>
   let count = $state([]);
-  let total = $derived(count.reduce((a, b) => a + b, 0));
+  // let total = $derived(count.reduce((a, b) => a + b, 0));
+  let total = $derived.by(() => {
+    let total = 0;
+    for (let i = 0; i < count.length; i++) {
+      total += count[i];
+    }
+    return total;
+  });
 
   const increment = () => {
     count.push(1);
