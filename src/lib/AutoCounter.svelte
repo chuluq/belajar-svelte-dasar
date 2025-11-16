708ff8ae8b0d2ce0e7c37829e9ee5aa28ca9b6fb
Chuluq <choirulchuluq10@gmail.com>
2025-11-16 09:45:17 +0700

    effect

---
+++ src/lib/AutoCounter.svelte
@@ -0,0 +1,19 @@
+<script>
+  let counter = $state(0);
+  let interval = $state(1000);
+
+  function increment() {
+    interval += 1000;
+  }
+
+  $effect(() => {
+    const id = setInterval(() => {
+      counter++;
+    }, interval);
+
+    return () => clearInterval(id);
+  });
+</script>
+
+<h1>Counter: {counter}</h1>
+<button onclick={increment}>Increment interval: {interval} ms</button>
