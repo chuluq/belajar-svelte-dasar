0e14906a1d394e8372ceb7b834a8651de1e5ef3d
Chuluq <choirulchuluq10@gmail.com>
2025-11-16 18:17:42 +0700

    props

---
+++ src/lib/UserApp.svelte
@@ -0,0 +1,20 @@
+<script>
+  import UserRow from "./UserRow.svelte";
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Address</th>
+    </tr>
+  </thead>
+  <tbody>
+    <UserRow id="1" name="John" address="Jalan Cinta" />
+    <UserRow id="1" name="John" address="Jalan Cinta" />
+    <UserRow id="1" name="John" address="Jalan Cinta" />
+    <UserRow id="1" name="John" address="Jalan Cinta" />
+    <UserRow id="1" name="John" address="Jalan Cinta" />
+  </tbody>
+</table>
