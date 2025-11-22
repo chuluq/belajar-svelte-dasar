946b495509792b23ca46917e2e0e94c2886c0f2a
Chuluq <choirulchuluq10@gmail.com>
2025-11-22 22:11:39 +0700

    special element

---
+++ src/lib/Mouse.svelte
@@ -0,0 +1,15 @@
+<script>
+  const mouse = $state({
+    x: 0,
+    y: 0,
+  });
+
+  function handleMouseMove(event) {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+  }
+</script>
+
+<svelte:body onmousemove={handleMouseMove} />
+
+<h1>Mouse position: {mouse.x}, {mouse.y}</h1>
