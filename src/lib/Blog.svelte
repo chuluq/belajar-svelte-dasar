3bfc855a7fa022b20898d2e35acd5a9fcbcd7b5d
Chuluq <choirulchuluq10@gmail.com>
2025-11-22 22:04:53 +0700

    lifecycle hook

--- src/lib/Blog.svelte
+++ src/lib/Blog.svelte
@@ -1,20 +1,21 @@
 <script>
   import Layout from "./Layout.svelte";
+  import { onMount } from "svelte";
+
+  let blog = $state({
+    title: "",
+    content: "",
+    footer: "",
+  });
+
+  onMount(async () => {
+    const response = await fetch("/blog.json");
+    blog = await response.json();
+  });
 </script>
 
-<Layout title="Belajar Svelte Dasar" footer="Copyright 2025">
+<Layout title={blog.title} footer={blog.footer}>
   {#snippet content()}
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor earum
-      fugiat numquam excepturi mollitia laudantium cum facilis, nostrum maiores
-      repudiandae, obcaecati, aperiam odio id rerum nihil vel harum repellendus
-      consequuntur?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor earum
-      fugiat numquam excepturi mollitia laudantium cum facilis, nostrum maiores
-      repudiandae, obcaecati, aperiam odio id rerum nihil vel harum repellendus
-      consequuntur?
-    </p>
+    {@html blog.content}
   {/snippet}
 </Layout>
